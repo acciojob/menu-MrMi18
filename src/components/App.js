a9a12cf916918bd37e7a76f1fadf991d7f19c7bf
MrMi18 <is8388508@gmail.com>
2025-06-08 10:32:24 +0530

    All section removed

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <div id="main" >
      <h1>Our Menu</h1>
-     <button id = "filter-btn" >All</button>
+     {/* <button id = "filter-btn" >All</button> */}
       <button id = "filter-btn-1" onClick={() => filterHandler("Breakfast")} >Breakfast</button>
       <button id = "filter-btn-2" onClick={() => filterHandler("Lunch")} >Lunch</button>
       <button id = "filter-btn-3" onClick={() => filterHandler("Shakes")} >Shakes</button>
